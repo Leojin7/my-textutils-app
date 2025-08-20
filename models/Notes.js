546b5af35d26d640174582db40c5672a224bd1d6
Dev Ruhela <leo18jin@gmail.com>
2025-08-20 23:37:35 +0530

    Update Notes.js

--- models/Notes.js
+++ models/Notes.js
@@ -1,9 +1,7 @@
-
 const moongoose = require('mongoose');
 const { use } = require('react');
 const { Schema } = moongoose;
 const UserSchema = new moongoose.Schema({
-
 
   name: {
     type: String,
@@ -30,7 +28,7 @@ const UserSchema = new moongoose.Schema({
 
 });
 
-
 const User = moongoose.model('user', UserSchema);
 User.createIndexes(); // Ensure indexes are created for the User model
+
 module.exports = moongoose.model('User', UserSchema);
